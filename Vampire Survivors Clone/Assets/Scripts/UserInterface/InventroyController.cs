e0a06f65e7c028880d4e32b2c253fb2c07d1b9d3
Dustin Santoso <dustin.santoso025@gmail.com>
2025-05-01 19:14:33 -0700

    Inventory and Timer

---
+++ Vampire Survivors Clone/Assets/Scripts/UserInterface/InventroyController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InventroyController : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int slotCount;
+    public GameObject[] itemPrefabs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
