3aaa2e14449ba8e417747049e0c2d313e572b446
Dustin Santoso <dustin.santoso025@gmail.com>
2025-05-03 10:45:27 -0700

    Pause Menu + Timer
    Quit Button only works once the game is compiled

---
+++ Vampire Survivors Clone/Assets/Scripts/UserInterface/PauseMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
